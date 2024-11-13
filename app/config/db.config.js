72ccebd25e6c2fc4f8e86bcbd7f37c26ed2dd4b3
Thiare <thiarecis1102@gmail.com>
2024-11-13 13:01:02 -0300

    practica de consolidacion del modulo 7 finalizado

---
+++ app/config/db.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "user_db_bootcamp",
+    PASSWORD: "db_bootcamp",
+    DB: "db_bootcamp",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  };
+  
